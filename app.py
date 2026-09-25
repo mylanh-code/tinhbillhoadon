bb662d3a0bfcd9687148e107dba504d57de38cd4
mylanh-code <24030055@student.bdu.edu.vn>
2026-09-25 10:01:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from datetime import datetime
 import pandas as pd
 import streamlit as st
- 
+st.image("IMG_2935.jpeg")
 st.set_page_config(page_title="Order Nhà Hàng", layout="wide")
  
 # Đường dẫn file dữ liệu dùng chung trên máy chủ
